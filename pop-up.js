1517c32c1a5ec86d8ba6d9b9ffb9a5fac25b4f0f
Etiendo <32451923+Etiendo@users.noreply.github.com>
2017-10-17 17:28:52 +0200

    Version 0.1

---
+++ pop-up.js
@@ -0,0 +1,11 @@
+/*
+fonction ouvrir une fenêtre après 3 secondes
+*/
+
+var time = 3000;
+setTimeout(function(){ window.open("http://www.letopdelhumour.fr/wp-content/uploads/2016/08/pixiz-30-08-2015-11-27-37-300x300.jpg", "Lagaffe"); }, time);
+
+
+/*
+window.open(URL, name, specs, replace)
+*/
